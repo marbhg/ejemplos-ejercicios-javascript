3edb282182c44481a4b132ea90690d905a831497
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-12 22:17:05 +0100

    Actualizo
    Ejercicios Variables

---
+++ ejercicios/variables/ejercicio2.js
@@ -0,0 +1,33 @@
+// Creo la variable con los datos de mi pelicula favorita
+let titulo = "Rapido y furioso";
+let director = "Robert";
+let ano = "2010";
+let genero = "Mixto";
+let duracionenminutos = 230; //minutos
+let tienepremios = true;
+
+// Muestro los datos en español
+document.write("Mostrar datos de la Pelicula en Español <br>");
+document.write("Titulo de la Pelicula: " + titulo + "<br>");
+document.write("Directo de la Pelicula: " + director + "<br>");
+document.write("Año de lanzamiento: " + ano + "<br>");
+document.write("Genero: " + genero + "<br>");
+document.write("Duracion en minutos: " + duracionenminutos + " minutos<br>");
+document.write("¿Tiene premios? " + tienepremios + "<br><br>")
+
+
+// Muestro los datos en ingles 
+titulo = "Fast and furious";
+director = "Robert";
+ano = "2010";
+genero = "Mixed";
+duracionenminutos = 230;
+tienepremios = "true"
+
+document.write("Show movie data in English " + "<br>");
+document.write("Movie Title: " + titulo + "<br>");
+document.write("Movie Director: " + director + "<br>");
+document.write("Year of release: " + ano + "<br>");
+document.write("Gender: " + genero + "<br>");
+document.write("Duration in minutes: " + duracionenminutos + "minutos<br>");
+document.write("Has awards " + tienepremios + "<br>");
