6148b54f4c3fb803558f87cfdd8b08b25274ba12
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-12 20:11:01 +0100

    ACT
    Ejercicios

---
+++ ejercicios/variables/ejercicio1.js
@@ -0,0 +1,9 @@
+let nombre = "Marbelis";
+document.write(" nombre " + nombre);
+
+let apellido = "Hugo";
+document.write(" apellido " + apellido);
+
+let edad = 35;
+document.write(" edad " + edad);
+
