aab5d43e7a1299e441afd1eecdf68a0be085606e
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-12 23:29:54 +0100

    Ejercicio
    Act 5

--- ejercicios/variables/ejercicio5.js
+++ ejercicios/variables/ejercicio5.js
@@ -1,18 +1,18 @@
-
+//Declaro las variables de datos sobre el clima 
 let temperatura = 32;
 let humedad = "Moderada";
 let probalidaLluvia = "Alta";
-let 
+let esDedia = true;
+let velocidadViento = 500;
+
+// Reflejo el resultado de mis variables 
+documento.write("Temperatura del tiempo: " + temperatura + "<br>");
+document.write("Intensidad de la Humedad: " + humedad + "<br>");
+document.write("Probabilidad de lluvia " + probalidaLluvia + "<br>");
+document.write("¿Es de dia o de noche? " + esDedia + "<br>");
+document.write("Velocidad del viento: " + velocidadViento + "<br>");
 
 
 
 
 
-
-
-
-Temperatura actual 
-Humedad 
-Probabilidad de lluvia 
-¿Es de día? (verdadero o falso) 
-Velocidad del viento
