2fb09239dbc7592184065e6c68d1b163e3434104
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-12 20:56:05 +0100

    Actualizo
    Variables.

--- ejercicios/variables/ejercicio1.js
+++ ejercicios/variables/ejercicio1.js
@@ -1,9 +1,57 @@
+
+document.write("Datos sobre Mi" + "<br>");
+
 let nombre = "Marbelis";
-document.write(" nombre " + nombre);
+document.write(" Nombre: " + nombre + "<br>");
 
 let apellido = "Hugo";
-document.write(" apellido " + apellido);
+document.write(" Apellido: " + apellido + "<br>");
 
 let edad = 35;
-document.write(" edad " + edad);
+document.write(" Edad: " + edad + "<br>");
+
+let altura = 1.75;
+document.write(" Altura: " + altura + "<br>");
+
+let ciudad = "Madrid";
+document.write(" Ciudad: " + ciudad + "<br>");
+
+let tegustaLaplaya = "true"
+document.write(" ¿Le gusta la playa? " + gustaLaplaya + "<br>");
+
+let tegustalaMontaña = true;
+document.write(" ¿Legusta la montaña? " + gustalaMontaña + "<br>");
+
+let tegustaelChocolate = true;
+document.write(" ¿Le gusta el chocolate? " + gustaelChocolate + "<br>");
+
+let tieneMascotas = true;
+document.write(" ¿Tiene mascota? " + tieneMascota + "<br>");
+
+
+
+//Creo las variales sobre otras personas 
+nombre = "Joaquin";
+apellido = "Sanchez Mata";
+edad = 35;
+altura = 1.80;
+ciudad = "Madrid";
+tegustaLaplaya = "false";
+tegustalaMontaña = "true";
+tegustaelChocolate = "true";
+tieneMascotas = "true";
+
+//Muestro resultado de las variales
+document.write("Nombre: " + nombre + "<br>");
+document.write("Apellido: " + apellido + "<br>");
+document.write("Edad: " + edad + "<br>");
+document.write("Altura: " + altura + "<br>");
+document.write("Ciudad: " + ciudad + "<br>");
+document.write("¿Te gusta la playa? " + tegustaLaplaya + "<br>");
+document.write("¿Te gusta la Montaña? " + tegustalaMontaña + "<br>");
+document.write("¿Te gusta el Chocolate " + tegustaelChocolate + "<br>");
+document.write("¿Tienes Mascota? " + tieneMascota + "<br>");
+
+
+
 
