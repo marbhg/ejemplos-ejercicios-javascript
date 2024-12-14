6cfe5323c1676c3b0959875aafe31c7f2b52ad81
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-14 21:40:25 +0100

    Calculadora
    Act

---
+++ ejercicios/Operaciones/ejercicio1opera.js
@@ -0,0 +1,18 @@
+//Definimos los numeros
+let numero1 = 10;
+let numero2 = 4;
+
+//Creo la variable de suma, resta, multiplicacion y division
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+//Muestro resultado de las operaciones 
+document.write("La suma de: " + numero1 + "y " + numero2 + "es igual a: " + suma + "<br>");
+document.write("La resta de: " + numero1 + "y " + numero2 + "es igual a: " + resta + "<br>");
+document.write("La multiplicacion de; " + numero1 + "y " + numero2 + "es igual a: " + multiplicacion + "<br>");
+document.write("La division de: " + numero1 + "y " + numero2 + "es igual a: " + division + "<br>");
+
+
+
