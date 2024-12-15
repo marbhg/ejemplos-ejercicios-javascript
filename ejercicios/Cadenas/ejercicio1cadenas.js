614ae7b57736cf7a9cf25dcd879dbb7545c8a580
Marrbe <132088541+marbhg@users.noreply.github.com>
2024-12-15 22:17:39 +0100

    ejercicios
    nuevos

---
+++ ejercicios/Cadenas/ejercicio1cadenas.js
@@ -0,0 +1,17 @@
+//Creo una frase en una variable
+let frase = "vive tus sueños";
+
+//Muestro la longitud de la frase
+let longitudfrase = frase.length
+document.write("Muesto la longitud de la frase: " + longitudfrase + "<br>");
+
+//Muestto la frase en Mayuscula
+let fraseMayuscula = frase.toUpperCase;
+document.write("Frase en Mayuscula: " + fraseMayuscula + "<br>");
+
+//Muestro la frase en minuscula
+let fraseMinuscula = frase.toLowerCase;
+document.write("Frase en minuscula: " + fraseMinuscula + "<br>");
+
+//Reemplazar la frase por otra 
+let nuevaFrase = frase.replace
